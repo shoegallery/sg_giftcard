4c243cb9a6b07e72ccd9f0fbd18e0f9b98526b3c
shoegallery <87663656+shoegallery@users.noreply.github.com>
2022-05-23 04:19:06 +0800

    1

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import { registerRootComponent } from "expo";
 import "expo-asset";
 
 import App from "./App";
-import { name as appName } from "./app.json";
-AppRegistry.registerComponent(appName, () => App);
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
