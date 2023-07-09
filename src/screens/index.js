7dbd03010c16b927db58402c9be781c05c0dfe25
Tamir Ariunsukh <87663656+shoegallery@users.noreply.github.com>
2023-07-10 01:14:56 +0800

    .

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,11 +1,11 @@
-export { default as StartScreen } from "./StartScreen";
+
 export { default as LoginScreen } from "./LoginScreen";
 export { default as ProfileScreen } from "./ProfileScreen";
 export { default as LocationScreen } from "./LocationScreen";
 export { default as TabbarScreen } from "./TabbarScreen";
 export { default as TermScreen } from "./TermScreen";
 export { default as LoginAuthScreen } from "./LoginAuthScreen";
-export { default as ExpenseScreen } from "./ExpenseScreen";
+
 export { default as PurchaseScreen } from "./PurchaseScreen";
 export { default as TransferScreen } from "./TransferScreen";
 export { default as GetCouponScreen } from "./GetCouponScreen";
